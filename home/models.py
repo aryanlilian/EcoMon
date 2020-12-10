86234b25dcd3aaf7c8b8a28b05cac5215ab759c5
Bularu Lilian <aryanlilian@gmail.com>
2020-12-10 21:03:31 +0200

    Home App models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Newsletter(models.Model):
+    email = models.EmailField()
+    subscribed_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email

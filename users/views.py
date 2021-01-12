89a3441082929658cd9038525c2e63c8dc5d9ab4
Bularu Lilian <>
2021-01-12 17:52:10 +0200

    Fixing bugs on JS

--- users/views.py
+++ users/views.py
@@ -1,13 +1,14 @@
-from .models import Income, Spending
 from django.urls import reverse_lazy
 from datetime import datetime
 from . import constants
 from django.views import View
 from django.shortcuts import render
 from django.views.generic import CreateView  #, DeleteView
-from .forms import (
-    IncomeCreateForm,
-    SpendingCreateForm,
+from .forms import IncomeCreateForm, SpendingCreateForm
+from .models import (
+    Income,
+    Spending,
+    Profile,
 )
 from .utils_functions import (
     assembly,
@@ -24,38 +25,25 @@ class DashboardListView(View):
     def get(self, request):
         number_of_days = days_of_month(
             datetime.now().year, datetime.now().month)
-        # currency = Currency.objects.get(user=request.user)
         incomes = Income.objects.filter(
             user=request.user, created_date__year=datetime.now().year, created_date__month=datetime.now().month)
         spendings = Spending.objects.filter(
             user=request.user, created_date__year=datetime.now().year, created_date__month=datetime.now().month)
-        max_income = max_amount(incomes)
-        max_spending = max_amount(spendings)
-        total_incomes = round(assembly(incomes), 2)
-        total_spendings = round(assembly(spendings), 2)
-        total_savings = round(total_incomes - total_spendings, 2)
-        spendings_percent = percentages_of_incomes(
-            total_incomes, total_spendings)
-        savings_percent = percentages_of_incomes(
-            total_incomes, total_savings)
-        avg_incomes = daily_avg(total_incomes, number_of_days)
-        avg_spendings = daily_avg(total_spendings, number_of_days)
-        avg_savings = daily_avg(total_savings, number_of_days)
         context = {
             'title': 'Dashboard',
             'spendings': spendings,
             'incomes': incomes,
-            # 'currency': currency.currency,
-            'max_income': max_income,
-            'max_spending': max_spending,
-            'total_incomes': total_incomes,
-            'total_spendings': total_spendings,
-            'total_savings': total_savings,
-            'spendings_percent': spendings_percent,
-            'savings_percent': savings_percent,
-            'avg_incomes': avg_incomes,
-            'avg_spendings': avg_spendings,
-            'avg_savings': avg_savings,
+            'currency' : Profile.objects.get(user=self.request.user).currency,
+            'max_income' : max_amount(incomes),
+            'max_spending': max_amount(spendings),
+            'total_incomes': round(assembly(incomes), 2),
+            'total_spendings': round(assembly(spendings), 2),
+            'total_savings': round(assembly(incomes) - assembly(spendings), 2),
+            'spendings_percent': percentages_of_incomes(assembly(incomes), assembly(spendings)),
+            'savings_percent': percentages_of_incomes(assembly(incomes), round(assembly(incomes) - assembly(spendings), 2)),
+            'avg_incomes': daily_avg(assembly(incomes), number_of_days),
+            'avg_spendings': daily_avg(assembly(spendings), number_of_days),
+            'avg_savings': daily_avg(assembly(incomes) - assembly(spendings), number_of_days),
         }
         return render(request, 'users/dashboard.html', context)
 
@@ -84,6 +72,7 @@ class IncomeCreateListView(CreateView):
         context['primary_color'] = 'primary'
         context['objects'] = incomes
         context['total_sum'] = total_incomes
+        context['currency'] = Profile.objects.get(user=self.request.user).currency
         context['total_sum_last_month'] = total_incomes_last_month
         context['date'] = datetime.now()
         return context
@@ -118,6 +107,7 @@ class SpendingCreateListView(CreateView):
         context['primary_color'] = 'danger'
         context['objects'] = spendings
         context['total_sum'] = total_spendings
+        context['currency'] = Profile.objects.get(user=self.request.user).currency
         context['total_sum_last_month'] = total_spendings_last_month
         context['date'] = datetime.now()
         return context

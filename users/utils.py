c2a582566ebd00f5fec0705fe4da15edb5363403
Bularu Lilian <aryanlilian@gmail.com>
2021-01-24 22:32:20 +0200

    Rewriting utils.py recursion functions code

--- users/utils.py
+++ users/utils.py
@@ -25,36 +25,27 @@ def max_amount(objs=None):
 
 def check_recurrent_or_new(user, objs, model):
     for item in objs:
-        if item.created_date.month == 12:
-            date = datetime(
-                    item.created_date.year + 1, 1, item.created_date.day,
-                    item.created_date.hour, item.created_date.minute, item.created_date.second,
-                    item.created_date.microsecond
-                )
-        else:
-            date = datetime(
-                    item.created_date.year, item.created_date.month + 1, item.created_date.day,
-                    item.created_date.hour, item.created_date.minute, item.created_date.second,
-                    item.created_date.microsecond
-                )
-        obj, created = model.objects.get_or_create(
+        date_test = item.created_date.month == 12
+        date = datetime(
+            item.created_date.year + 1 if date_test else item.created_date.year,
+            1 if date_test else item.created_date.month + 1, item.created_date.day,
+            item.created_date.hour, item.created_date.minute, item.created_date.second,
+            item.created_date.microsecond
+        )
+        obj,created = model.objects.get_or_create(
             user=user, name=item.name, amount=item.amount,
             created_date=date, category=item.category, recurrent=True
         )
 
 
 def delete_recurrent_object(user, obj, model):
-    if obj.created_date.month == 12:
-        date = datetime(
-                obj.created_date.year + 1, 1, obj.created_date.day,
-                obj.created_date.hour, obj.created_date.minute, obj.created_date.second,
-                obj.created_date.microsecond
-            )
-    else:
-        date = datetime(
-                obj.created_date.year, obj.created_date.month + 1, obj.created_date.day,
-                obj.created_date.hour, obj.created_date.minute, obj.created_date.second,
-                obj.created_date.microsecond)
+    date_test = obj.created_date.month == 12
+    date = datetime(
+        obj.created_date.year + 1 if date_test else obj.created_date.year,
+        1 if date_test else obj.created_date.month + 1, obj.created_date.day,
+        obj.created_date.hour, obj.created_date.minute, obj.created_date.second,
+        obj.created_date.microsecond
+    )
     object = model.objects.get(
         user=user, name=obj.name, amount=obj.amount,
         created_date=date, category=obj.category, recurrent=True

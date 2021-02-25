62ffbbc16731ef7d478a47a93e98002c4b293db9
Bularu Lilian <aryanlilian@gmail.com>
2021-02-25 11:24:17 +0200

    combined similar functions in a single general function

--- users/helpers.py
+++ users/helpers.py
@@ -4,131 +4,69 @@ from datetime import datetime
 from django.http import JsonResponse
 
 
-def incomes_chart_area(request, pk=None):
-    incomes_data, date_distance, checks = [], -1, 12
-    account = Account.objects.get(id=pk) #if pk else None
-    first_income = Income.objects.filter(user=request.user, account=account).first() if account else Income.objects.filter(user=request.user).first()
-    last_income = Income.objects.filter(user=request.user, account=account).last() if account else Income.objects.filter(user=request.user).last()
-    if first_income and last_income:
-        date_distance = (last_income.created_date - first_income.created_date).days
+def chart_area(request, pk=None):
+    data, date_distance, checks = [], -1, 12
+    account = Account.objects.get(id=pk) if pk else None
+    object = Income if '/users/dashboard/incomes-chart-area/' in request.get_full_path() else Spending
+    first_object = object.objects.filter(user=request.user, account=account).first() if account else object.objects.filter(user=request.user).first()
+    last_object = object.objects.filter(user=request.user, account=account).last() if account else object.objects.filter(user=request.user).last()
+    if first_object and last_object:
+        date_distance = (last_object.created_date - first_object.created_date).days
     if  1 <= date_distance <= 365:
-        year, month = first_income.created_date.year, first_income.created_date.month
+        year, month = first_object.created_date.year, first_object.created_date.month
         while checks:
             date = datetime(year, month, 1)
             if month == 12:
-                incomes = Income.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                incomes_data.append({date.strftime('%b'): assembly(incomes)})
+                objects = object.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
+                data.append({date.strftime('%b'): assembly(objects)})
                 month = 1
                 year += 1
             else:
-                incomes = Income.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                incomes_data.append({date.strftime('%b'): assembly(incomes)})
+                objects = object.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
+                data.append({date.strftime('%b'): assembly(objects)})
                 month += 1
             checks -= 1
     elif date_distance > 0:
-        year, month = last_income.created_date.year - 1, last_income.created_date.month + 1
+        year, month = last_object.created_date.year - 1, last_object.created_date.month + 1
         while checks:
             date = datetime(year, month, 1)
             if month == 12:
-                incomes = Income.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                incomes_data.append({date.strftime('%b'): assembly(incomes)})
+                objects = object.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
+                data.append({date.strftime('%b'): assembly(objects)})
                 month = 1
                 year += 1
             else:
-                incomes = Income.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                incomes_data.append({date.strftime('%b'): assembly(incomes)})
+                objects = object.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
+                data.append({date.strftime('%b'): assembly(objects)})
                 month += 1
             checks -= 1
     elif date_distance == 0:
-        year, month = first_income.created_date.year, first_income.created_date.month
+        year, month = first_object.created_date.year, first_object.created_date.month
         while checks:
             date = datetime(year, month, 1)
             if month == 12:
-                incomes = Income.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                incomes_data.append({date.strftime('%b'): assembly(incomes)})
+                objects = object.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
+                data.append({date.strftime('%b'): assembly(objects)})
                 month = 1
                 year += 1
             else:
-                incomes = Income.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                incomes_data.append({date.strftime('%b'): assembly(incomes)})
+                objects = object.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
+                data.append({date.strftime('%b'): assembly(objects)})
                 month += 1
             checks -= 1
-    return JsonResponse(incomes_data, safe=False)
+    return JsonResponse(data, safe=False)
 
 
-def spendings_chart_area(request, pk=None):
-    account = Account.objects.get(id=pk)
-    spendings_data, date_distance, checks = [], -1, 12
-    first_spending = Spending.objects.filter(user=request.user, account=account).first() if account else Spending.objects.filter(user=request.user).first()
-    last_spending = Spending.objects.filter(user=request.user, account=account).last() if account else Spending.objects.filter(user=request.user).last()
-    if first_spending and last_spending:
-        date_distance = (last_spending.created_date - first_spending.created_date).days
-    if 1 <= date_distance <= 365:
-        year, month = first_spending.created_date.year, first_spending.created_date.month
-        while checks:
-            date = datetime(year, month, 1)
-            if month == 12:
-                spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                spendings_data.append({date.strftime('%b'): assembly(spendings)})
-                month = 1
-                year += 1
-            else:
-                spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                spendings_data.append({date.strftime('%b'): assembly(spendings)})
-                month += 1
-            checks -= 1
-    elif date_distance > 0:
-        year, month = last_spending.created_date.year - 1, last_spending.created_date.month + 1
-        while checks:
-            date = datetime(year, month, 1)
-            if month == 12:
-                spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                spendings_data.append({date.strftime('%b'): assembly(spendings)})
-                month = 1
-                year += 1
-            else:
-                spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                spendings_data.append({date.strftime('%b'): assembly(spendings)})
-                month += 1
-            checks -= 1
-    elif date_distance == 0:
-        year, month = first_spending.created_date.year, first_spending.created_date.month
-        while checks:
-            date = datetime(year, month, 1)
-            if month == 12:
-                spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                spendings_data.append({date.strftime('%b'): assembly(spendings)})
-                month = 1
-                year += 1
-            else:
-                spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=year, created_date__month=month)
-                spendings_data.append({date.strftime('%b'): assembly(spendings)})
-                month += 1
-            checks -= 1
-    return JsonResponse(spendings_data, safe=False)
-
-
-def incomes_chart_pie(request, pk=None):
-    account = Account.objects.get(id=pk)
-    incomes = Income.objects.filter(user=request.user, account=account, created_date__year=datetime.now().year, created_date__month=datetime.now().month)
-    total_incomes = assembly(incomes)
-    incomes_data, categories = [], ['Salary', 'Awards', 'Grants', 'Sale', 'Dividents', 'Rental', 'Refunds', 'Coupons', 'Lottery', 'Capital', 'Investments', 'Gift', 'Others']
+def chart_pie(request, pk=None):
+    account = Account.objects.get(id=pk) if pk else None
+    object = Income if '/users/dashboard/incomes-chart-pie/' in request.get_full_path() else Spending
+    objects = object.objects.filter(user=request.user, account=account, created_date__year=datetime.now().year, created_date__month=datetime.now().month)
+    total_objects = assembly(objects)
+    data = []
+    categories = ['Salary', 'Awards', 'Grants', 'Sale', 'Dividents', 'Rental', 'Refunds', 'Coupons', 'Lottery', 'Capital', 'Investments', 'Gift', 'Others'] if object is Income else ['Utilities', 'Rent', 'Invoices', 'Shopping', 'Food', 'Education', 'Fun', 'Investment', 'Others']
     for category in categories:
-        category_incomes = Income.objects.filter(user=request.user, account=account, created_date__year=datetime.now().year, created_date__month=datetime.now().month, category=category)
-        total_category_incomes = assembly(category_incomes)
-        if total_incomes and total_category_incomes:
-            incomes_data.append({category: round((total_category_incomes / total_incomes) * 100, 2)})
-    return JsonResponse(incomes_data, safe=False)
-
-
-def spendings_chart_pie(request, pk):
-    account = Account.objects.get(id=pk)
-    spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=datetime.now().year, created_date__month=datetime.now().month)
-    total_spendings = assembly(spendings)
-    spendings_data, categories = [], ['Utilities', 'Rent', 'Invoices', 'Shopping', 'Food', 'Education', 'Fun', 'Investment', 'Others']
-    for category in categories:
-        category_spendings = Spending.objects.filter(user=request.user, account=account, created_date__year=datetime.now().year, created_date__month=datetime.now().month, category=category)
-        total_category_spendings = assembly(category_spendings)
-        if total_spendings and total_category_spendings:
-            spendings_data.append({category: round((total_category_spendings / total_spendings) * 100, 2)})
-    return JsonResponse(spendings_data, safe=False)
+        category_objects = object.objects.filter(user=request.user, account=account, created_date__year=datetime.now().year, created_date__month=datetime.now().month, category=category)
+        total_category_objects = assembly(category_objects)
+        if total_objects and total_category_objects:
+            data.append({category: round((total_category_objects / total_objects) * 100, 2)})
+    return JsonResponse(data, safe=False)
--- users/urls.py
+++ users/urls.py
@@ -7,18 +7,17 @@ from .views import (
     EmailVerificationView, SendOrVerifyEmailVerificationView
 )
 from .helpers import (
-    incomes_chart_area, incomes_chart_pie, spendings_chart_area,
-    spendings_chart_pie,
+    chart_area, chart_pie
 )
 
 urlpatterns = [
     path('accounts/', AccountsListView.as_view(), name='accounts'),
     path('dashboard/<int:pk>/', DashboardView.as_view(), name='dashboard'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('dashboard/incomes-chart-area/<int:pk>/', incomes_chart_area, name='incomes-chart-area'),
-    path('dashboard/incomes-chart-pie/<int:pk>/', incomes_chart_pie, name='incomes-chart-pie'),
-    path('dashboard/spendings-chart-area/<int:pk>/', spendings_chart_area, name='spendings-chart-area'),
-    path('dashboard/spendings-chart-pie/<int:pk>/', spendings_chart_pie, name='spendings-chart-pie'),
+    path('dashboard/incomes-chart-area/<int:pk>/', chart_area, name='incomes-chart-area'),
+    path('dashboard/incomes-chart-pie/<int:pk>/', chart_pie, name='incomes-chart-pie'),
+    path('dashboard/spendings-chart-area/<int:pk>/', chart_area, name='spendings-chart-area'),
+    path('dashboard/spendings-chart-pie/<int:pk>/', chart_pie, name='spendings-chart-pie'),
     path('incomes/new/<int:pk>/', IncomesCreateListView.as_view(), name='incomes'),
     path('spendings/new/<int:pk>/', SpendingsCreateListView.as_view(), name='spendings'),
     path('accounts/new/', AccountCreateView.as_view(), name='add-accounts'),
